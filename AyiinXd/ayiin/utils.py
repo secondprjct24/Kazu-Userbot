a74a2e498e15e30836d79b0310b0696f838ab5b3
bluefloyd24 <ncimsaykuw019@gmail.com>
2024-08-14 18:10:23 +0700

    Update utils.py

--- AyiinXd/ayiin/utils.py
+++ AyiinXd/ayiin/utils.py
@@ -228,7 +228,7 @@ async def autobot():
             f"BERHASIL MEMBUAT ASSISTANT BOT KAMU DENGAN USERNAME @{username}")
         await bot.send_message(
             BOTLOG_CHATID,
-            "**restarting bot. special thanks to @disinikazu/@html12text**",
+            "**restarting bot. special thanks to @disinikazu for this great repository**",
         )
         heroku_var["BOT_TOKEN"] = token
         heroku_var["BOT_USERNAME"] = f"@{username}"
